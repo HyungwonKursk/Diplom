728f09d102beaef767cede52d75ffbb7fa0fb45c
Hyungwon <Romantsova1205@yande.ru>
2020-10-19 13:09:56 +0300

    Добавление класса Групп

---
+++ Xaminals/Models/Group.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xaminals.Models
+{
+    class Group
+    {
+        ///<summary>
+        ///numb-номер группы ключ
+        ///prof-профиль подготовки
+        ///col_st-кол-во студентов в группе
+        ///
+        ///course-курс
+        ///</summary>
+
+        private int numb;
+        public int Numb { get=>numb; set=> numb = value; }
+        private string prof;
+        public string Prof { get => prof; set=>prof=value; }
+
+        private int col_st;
+        public int Col_St { get => col_st; set => col_st = value; }
+
+        private int course;
+        public int Course { get => course; set => course = value; }
+
+        public Group()
+        {
+            numb = 0; 
+            prof = ""; 
+            col_st = 0; 
+            course = 0;
+        }
+        public Group(int n, string p, int cs, int c)
+        {
+            numb = n; 
+            prof = p; 
+            col_st = cs; 
+            course = c;
+        }
+
+        public Group(Group group)
+        {
+            numb = group.numb;
+            prof = group.prof;
+            col_st = group.col_st;
+            course = group.course;
+        }
+    }
+}
